846f5c91b55730280ec490d358ba2a6d2ae3c793
WangKang1555 <488376359@qq.com>
2023-03-15 00:49:27 +0800

    2023-03-15-0:49:27

--- gitpush.bat
+++ gitpush.bat
@@ -1,15 +1,12 @@
 @echo off
 echo Start push your code......
 @echo on
-cd ./
-
 git add ./
 @echo off
 set d=%date:~8,2%
 set m=%date:~5,2%
 set y=%date:~0,4%
 set t=%time:~0,-3%
-,2%
 set info=%y%-%m%-%d%-%t%
 @echo on
 git commit -m %info: =%
